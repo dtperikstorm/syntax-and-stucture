cb79ee9e6637f635ed43410b7f7d1742d2f2eaa9
dtperikstorm <dtperikstorm@gmail.com>
2020-10-19 12:14:00 +0200

    git init

---
+++ syntaxAndStucture/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace syntaxAndStucture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Good Bye World!");
+        }
+    }
+}
